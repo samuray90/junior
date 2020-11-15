f0a5729c979eea9c887f8a6a22676f4dc2350c98
samuray90 <ionutsdaniel53@gmail.com>
2020-11-15 15:52:49 +0000

    easy start

---
+++ this-cv/src/component/app/app.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid } from './app.style'
+
+const App = () => {
+  return (
+    <Grid>
+    </Grid>
+  )
+};
+
+
+export { App }
---
+++ this-cv/src/component/app/app.style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Grid = styled.div `
+display: grid;
+grid-template-columns: repeat(4, 1fr)
+grid-gap: 11px;
+margin: 0 36px;
+`
