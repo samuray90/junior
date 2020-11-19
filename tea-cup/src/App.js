bd5523074b84be1e2a723bd6f572af6efa408ad1
samuray90 <ionutsdaniel53@gmail.com>
2020-11-19 11:21:11 +0000

    next

--- tea-cup/src/App.js
+++ tea-cup/src/App.js
@@ -17,11 +17,12 @@ const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
-        <button onClick={() => setCounter(counter +1)} className="search-button" type="submit">{counter}
+        <button className="search-button" type="submit">Search
         </button>
       </form>
+      <h1 onClick={() => setCounter(counter +1)}>{counter}</h1>
     </div>
-  )
-}
+  );
+};
 
 export { App }
