db3137c6c5f1b36ca5fb716c020663649d306e3f
samuray90 <ionutsdaniel53@gmail.com>
2020-11-19 11:01:31 +0000

    learning counter and using APP_ID+KEY

---
+++ tea-cup/src/App.js
@@ -0,0 +1,27 @@
+import React, {useEffect ,useState} from "react"
+import "./App.css"
+
+const App = () => {
+
+  const APP_ID = "fa6fdf86"
+  const APP_KEY = "83c74dd3d52d4f1f5af6fbc83bbcbb30"
+const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    console.log('effect is running')
+  });
+
+  return(
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button onClick={() => setCounter(counter +1)} className="search-button" type="submit">{counter}
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export { App }
